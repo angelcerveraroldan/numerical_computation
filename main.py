df39e1da2b840ef1ce3e5442b5d7343bc2df1d03
Angel Cervera Roldan <angelcr@Angels-MacBook-Pro.local>
2024-04-11 10:40:29 +0100

    rref

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import numpy as np
+
+from linear_systems.gaussian import reduced_form
+
+
 def main(): 
-    pass
+    reduced_form(np.array([
+        [3.0, 6, 9, 39],
+        [2.0, 5, -2, 3],
+        [1.0, 3, -1, 2]
+    ]))
 
 if __name__ == "__main__": 
     main()
