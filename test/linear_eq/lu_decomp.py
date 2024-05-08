c9d75fbf8752ffbcce22bea62a937417b983be2b
Maciej Kwasnik <117639273+TEDiks2003@users.noreply.github.com>
2024-05-09 00:49:10 +0100

    LU decomposition no PP and added add_rows_scalar to matrix.py (#11)

--- test/linear_eq/lu_decomp.py
+++ test/linear_eq/lu_decomp.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 
 from src.constants import DEFAULT_DELTA
-from src.linear_eq.lu_decomp import lu_decomp, solve, solve_upper_triangular
+from src.linear_eq.lu_decomp import lu_decomp, solve, solve_upper_triangular, lu_decomp_no_pp
 
 
 class LuDecompTest(unittest.TestCase):
@@ -19,6 +19,31 @@ class LuDecompTest(unittest.TestCase):
         for i in range(0, 3):
             for j in range(0, 3):
                 self.assertTrue(abs(matrix[i][j] - mult[i][j]) < DEFAULT_DELTA)
+
+    def test_lu_decomp_no_pp(self):
+        matrix = np.asfarray([
+            [-3, 2, -1],
+            [6, -6, 7],
+            [3, -4, 4]
+        ])
+        l, u = lu_decomp_no_pp(matrix)
+        l_e = [
+            [1, 0, 0],
+            [-2, 1, 0],
+            [-1, 1, 1]
+        ]
+        u_e = [
+            [-3, 2, -1],
+            [0, -2, 5],
+            [0, 0, -2]
+        ]
+        for r_1, r_2 in zip(l, l_e):
+            for x, y in zip(r_1,r_2):
+                self.assertEqual(x, y)
+
+        for r_1, r_2 in zip(u, u_e):
+            for x, y in zip(r_1,r_2):
+                self.assertEqual(x, y)
 
     def test_solve_upper(self):
         matrix = [
